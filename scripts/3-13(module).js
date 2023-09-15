2e427e42e3dee7d706f42434784b7c32909fd931
Seoyeon0203 <w2222@e-mirim.hs.kr>
2023-09-15 13:47:55 +0900

    chapter3

---
+++ scripts/3-13(module).js
@@ -0,0 +1,17 @@
+// function log(message) {
+//   console.log(message);
+// }
+
+// function error(message) {
+//   console.log(message);
+// }
+
+export function log(message) {
+  console.log(message);
+}
+
+export function error(message) {
+  console.log(message);
+}
+
+export const PI = 3.14;
